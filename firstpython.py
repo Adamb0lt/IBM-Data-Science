59906ab1ad5dd4fc7ec2fa59e6dbe9e4f4ec8f19
Adam Walters <122646712+Adamb0lt@users.noreply.github.com>
2023-03-26 02:09:17 -0400

    test
    random code

---
+++ firstpython.py
@@ -0,0 +1,11 @@
+#Display output
+
+print("Hello World")
+gg = 0
+lol = input("say yes or no")
+if lol.lowercase()== "yes":
+  print("Thanks, for input")
+else:
+  gg+=1
+  print("Thanks anyways")
+          
